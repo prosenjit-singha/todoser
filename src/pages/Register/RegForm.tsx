147caf2f389364dcc8551cf2cc3d0998b954310a
PJAheibam <prosenjitsingha28@gmail.com>
2022-12-27 20:32:13 +0600

    password visibility toggle option added

--- src/pages/Register/RegForm.tsx
+++ src/pages/Register/RegForm.tsx
@@ -1,15 +1,22 @@
+import { useState } from "react";
 import {
   Paper,
   Typography,
   TextField,
   Stack,
   Button,
-  Divider,
+  InputAdornment,
+  IconButton,
 } from "@mui/material";
 import { FcGoogle } from "react-icons/fc";
+import {
+  MdOutlineVisibility as VisibilityIcon,
+  MdOutlineVisibilityOff as VisibilityOffIcon,
+} from "react-icons/md";
 import OrDivider from "./OrDivider";
 
 function RegForm() {
+  const [showPass, setShowPass] = useState(false);
   return (
     <Paper
       elevation={3}
@@ -19,10 +26,64 @@ function RegForm() {
         Register
       </Typography>
       <Stack spacing={2} mt={3}>
-        <TextField label="Full Name" size="small" fullWidth />
-        <TextField label="Email" size="small" fullWidth />
-        <TextField label="Password" size="small" fullWidth />
-        <TextField label="Confirm Password" size="small" fullWidth />
+        <TextField
+          name="fullName"
+          label="Full Name"
+          size="small"
+          fullWidth
+          autoComplete="off"
+          placeholder="John Cina"
+        />
+        <TextField
+          name="email"
+          label="Email"
+          type="email"
+          size="small"
+          fullWidth
+          placeholder="john@example.com"
+        />
+        <TextField
+          name="password"
+          type={showPass ? "text" : "password"}
+          placeholder="password"
+          label="Password"
+          size="small"
+          fullWidth
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  onClick={() => setShowPass(!showPass)}
+                  aria-label="toggle password visibility"
+                  edge="end"
+                >
+                  {showPass ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+        <TextField
+          name="confirmPassword"
+          label="Confirm Password"
+          type={showPass ? "text" : "password"}
+          placeholder="password"
+          size="small"
+          fullWidth
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  onClick={() => setShowPass(!showPass)}
+                  aria-label="toggle password visibility"
+                  edge="end"
+                >
+                  {showPass ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
         <Button type="submit" variant="outlined">
           Register
         </Button>
